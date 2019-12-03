2f9fd0e3e903785671a4a50ee97df7eea4f37d71
everpcpc <git@everpcpc.com>
2019-12-03 16:19:52 +0800

    complete single host client

--- src/client.rs
+++ src/client.rs
@@ -1,57 +1,69 @@
-
-extern crate hyper;
-
-use std::io::{self, Write};
+use std::str;
+use std::sync::mpsc::Sender;
 
 use hyper::rt::{self, Future, Stream};
 use hyper::Client;
+use url::form_urlencoded;
 
 
-fn fetch_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
-    let client = Client::new();
-
-    client
-        .get(url)
-        .and_then(|res| {
-            println!("Response: {}", res.status());
-            println!("Headers: {:#?}", res.headers());
-
-            res.into_body().for_each(|chunk| {
-                io::stdout().write_all(&chunk)
-                    .map_err(|e| panic!("example expects stdout is open, error={}", e))
-            })
-        })
-        .map(|_| {
-            println!("\n\nDone.");
-        })
-        .map_err(|err| {
-            eprintln!("Error {}", err);
-        })
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Result<'a> {
+    fd: u8,
+    stdout: &'a str,
+    stderr: &'a str,
+    time_cost: f64,
+    start_time: f64,
+    exit_code: i32,
+    error: String,
 }
 
-pub fn realtime_run_command(host: &str, port: u32, command: &str, arguments: Vec<&str>) -> i32 {
-    println!("host: {}", host);
-    println!("port: {}", port);
-    println!("command: {}", command);
-    for a in &arguments {
-        println!("argument: {}", a);
+fn build_url(host: &str, port: u32, command: &str, arguments: Vec<&str>) -> String {
+    let mut param_builder = form_urlencoded::Serializer::new(String::new());
+    param_builder.append_pair("chunked", "1");
+    if !arguments.is_empty() {
+        param_builder.append_pair("argument", arguments.join(" ").as_str());
     }
+    let param = param_builder.finish();
 
-    let url = format!("http://{}:{}/command/{}", host, port, command)
-        .parse()
-        .unwrap();
-
-    rt::run(fetch_url(url));
-
-    0
+    format!(
+        "http://{}:{}/command/{}?{}",
+        host,
+        port,
+        command,
+        param.as_str()
+    )
 }
 
-pub fn remote_run_in_parallel(
-    hosts: Vec<&str>,
-    port: u32,
-    command: &str,
-    arguments: Vec<&str>,
-) -> Vec<i32> {
+fn parse_chunk(s: &str) -> (i8, &str) {
+    let mut v = s.splitn(2, "> ");
+    let fd: i8 = v.next().unwrap().parse().unwrap();
+    let line = v.next().unwrap();
+    (fd, line)
+}
+
+// {"time_cost": 0.029803991317749023, "start_time": 1575272652.624216, "exit_code": 0, "stderr": "", "stdout": " 15:44:12 up 105 days, 23:41,  8 users,  load average: 7.77, 10.36, 6.26\n"}
+
+pub async fn rt_run(host: &str, port: u32, command: &str, arguments: Vec<&str>, tx: Sender<(i8, String)>) -> () {
+    let url = build_url(host, port, command, arguments).parse().unwrap();
+    rt::run({
+        let client = Client::new();
+        client
+            .get(url)
+            .and_then(move |res| {
+                res.into_body().for_each(move |chunk| {
+                    let b = &chunk.into_bytes();
+                    let (fd, line) = parse_chunk(str::from_utf8(b).unwrap());
+                    tx.send((fd, String::from(line))).unwrap();
+                    Ok(())
+                })
+            })
+            .map_err(|err| {
+                println!("Error: {}", err);
+            })
+    });
+}
+
+pub async fn run_parallel(hosts: Vec<&str>, port: u32, command: &str, arguments: Vec<&str>, tx: Sender<(String, i8, String)>) -> () {
     for h in &hosts {
         println!("host: {}", h);
     }
@@ -60,5 +72,4 @@ pub fn remote_run_in_parallel(
     for a in &arguments {
         println!("argument: {}", a);
     }
-    [0].to_vec()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 #[macro_use]
 extern crate clap;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
 mod client;
+
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+
+use futures::executor::LocalPool;
 
 fn main() {
     use clap::App;
@@ -36,16 +46,42 @@ fn main() {
         });
     }
 
-    let mut exit_code: i32 = 0;
+    let exit_code: i32;
+    let mut pool = LocalPool::new();
+
     if host.contains(",") {
         let hosts: Vec<&str> = host.split(",").collect();
-        let codes = client::remote_run_in_parallel(hosts, port, command, arguments);
-        if !codes.iter().all(|&x| x == 0) {
-            exit_code = 1;
-        }
+        let (tx, rx): (Sender<(String, i8, String)>, Receiver<(String, i8, String)>) = mpsc::channel();
+        pool.run_until(client::run_parallel(hosts, port, command, arguments, tx));
+        exit_code = print_multple_hosts_result(rx);
     } else {
-        exit_code = client::realtime_run_command(host, port, command, arguments);
+        let (tx, rx): (Sender<(i8, String)>, Receiver<(i8, String)>) = mpsc::channel();
+        pool.run_until(client::rt_run(host, port, command, arguments, tx));
+        exit_code = print_result(rx);
     }
 
     std::process::exit(exit_code);
+}
+
+
+fn print_result(rx: Receiver<(i8, String)>) -> i32 {
+    let mut finishied = false;
+    let mut ret = 0;
+    while !finishied {
+        let (fd, line) = rx.recv().unwrap_or((-1, String::from("UnfinishedCmd")));
+        match fd {
+            0 => {finishied = true},
+            1 => println!("{}", line),
+            2 => eprintln!("{}", line),
+            _ => {finishied = true},
+        }
+    }
+    ret
+}
+
+fn print_multple_hosts_result(rx: Receiver<(String, i8, String)>) -> i32 {
+           // if !codes.iter().all(|&x| x == 0) {
+        //     exit_code = 1;
+        // }
+    0
 }
