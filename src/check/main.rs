df3323870bedc5973c7802d9b22b8d8c4c86107a
everpcpc <git@everpcpc.com>
2019-12-08 16:49:06 +0800

    Complete redarrow-check

--- src/check/main.rs
+++ src/check/main.rs
@@ -1,7 +1,68 @@
 #[macro_use]
 extern crate clap;
 
+use std::f64::{INFINITY, NEG_INFINITY};
+
 use redarrow::webclient;
+
+/*
+Threshold format: [@]start:end
+
+Notes:
+  1. start <= end
+  2. start and ":" is not required if start=0
+  3. if range is of format "start:" and end is not specified, assume end is
+     infinity
+  4. to specify negative infinity, use "~"
+  5. alert is raised if metric is outside start and end range (inclusive of
+     endpoints)
+  6. if range starts with "@", then alert if inside this range (inclusive of
+     endpoints)
+
+For more detail and examples,
+    see http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT
+*/
+
+struct Threshold {
+    inside: bool,
+    start: f64,
+    end: f64,
+}
+
+impl Threshold {
+    fn parse(range_str: &str) -> Threshold {
+        let mut threshold = Threshold {
+            inside: false,
+            start: 0.0,
+            end: INFINITY,
+        };
+        let thredshold_str: &str;
+        if range_str.starts_with("@") {
+            threshold.inside = true;
+            thredshold_str = &range_str[1..];
+        } else {
+            thredshold_str = range_str;
+        }
+        if thredshold_str.contains(":") {
+            let mut v = thredshold_str.splitn(2, ":");
+            threshold.start = Threshold::parse_value(v.next().unwrap());
+            threshold.end = Threshold::parse_value(v.next().unwrap());
+        } else {
+            threshold.end = Threshold::parse_value(thredshold_str);
+        }
+        threshold
+    }
+
+    fn parse_value(value: &str) -> f64 {
+        if value == "~" {
+            NEG_INFINITY
+        } else if value == "" {
+            INFINITY
+        } else {
+            value.parse().unwrap()
+        }
+    }
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -20,12 +81,53 @@ fn main() {
             .collect();
     }
 
-    println!("host {}", host);
-    println!("command {}", command);
-    println!("arguments {:?}", arguments);
-
     let opts = webclient::Opts::new(host, 4205, command, arguments);
-    let ret = webclient::run_command(opts);
-    println!("{}", serde_json::to_string_pretty(&ret).unwrap())
+    let mut ret = webclient::run_command(opts);
 
+    if ret.error != "" {
+        ret.exit_code = 3;
+    }
+
+    if matches.is_present("raw") {
+        let output: String;
+        if ret.stdout != "" {
+            output = ret.stdout;
+        } else if ret.stderr != "" {
+            output = ret.stderr;
+        } else if ret.exit_code != 0 {
+            output = format!("Error: exit code is {}", ret.exit_code);
+        } else {
+            output = "OK".to_string();
+        }
+
+        std::process::exit({
+            println!("{}", output);
+            ret.exit_code
+        });
+    }
+
+    let value: f64 = ret.stdout.parse().unwrap();
+    println!("{}", ret.stdout);
+
+    let exit_code: i32;
+    if matches.is_present("critical") && should_alert(value, matches.value_of("critical").unwrap())
+    {
+        exit_code = 2;
+    } else if matches.is_present("warning")
+        && should_alert(value, matches.value_of("warning").unwrap())
+    {
+        exit_code = 1;
+    } else {
+        exit_code = 0;
+    }
+    std::process::exit(exit_code);
+}
+
+fn should_alert(value: f64, range_str: &str) -> bool {
+    let threshold = Threshold::parse(range_str);
+    if threshold.inside {
+        threshold.start <= value && value <= threshold.end
+    } else {
+        value < threshold.start || threshold.end < value
+    }
 }
