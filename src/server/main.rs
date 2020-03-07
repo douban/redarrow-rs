f49396020a495e5f6796099b4a807f7135d0d685
everpcpc <everpcpc@users.noreply.github.com>
2020-03-07 10:21:18 +0800

    chore: replace actix-web with warp (#22)

--- src/server/main.rs
+++ src/server/main.rs
@@ -2,14 +2,15 @@ use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
 
-use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
 use argh::FromArgs;
 use bytes::Bytes;
-use futures::executor;
 use futures::Stream;
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::mpsc;
+use warp::http::StatusCode;
+use warp::Filter;
 
-use redarrow::dispatcher::{read_config, Configs, RedarrowWaker};
+use redarrow::dispatcher::{read_config, Command, Configs, RedarrowWaker};
 use redarrow::{CommandParams, CommandResult};
 
 #[argh(description = "execute command for remote redarrow client")]
@@ -40,11 +41,10 @@ struct ServerArgs {
     workers: usize,
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init();
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
     let args: ServerArgs = argh::from_env();
-
     let configs = match read_config(args.config.as_str()) {
         Ok(c) => {
             log::info!("parsed {} commands, starting server...", &c.len());
@@ -52,71 +52,61 @@ async fn main() -> std::io::Result<()> {
         }
         Err(e) => {
             log::error!("parse config error: {}", e);
-            return Ok(());
+            return;
         }
     };
+    let configs = Arc::new(configs);
+    let configs = warp::any().map(move || configs.clone());
 
-    let (tx, rx) = std::sync::mpsc::channel::<&str>();
+    let handle_command = warp::path("command")
+        .and(warp::get())
+        .and(warp::path::param::<String>())
+        .and(warp::query::<CommandParams>())
+        .and(configs)
+        .and_then(handlers_command)
+        .with(warp::log("redarrow::http"));
 
-    let server = HttpServer::new(move || {
-        App::new()
-            .data(configs.clone())
-            .wrap(middleware::Logger::default())
-            .service(handlers_command)
-    })
-    .bind(format!("0.0.0.0:{}", args.port))?
-    .workers(args.workers)
-    .run();
+    let (tx, mut rx) = mpsc::channel::<&str>(10);
 
-    let srv = server.clone();
-    std::thread::Builder::new()
-        .name("signal receiver".to_string())
-        .spawn(move || loop {
-            let sig = rx.recv().unwrap_or("");
-            match sig {
-                "TERM" => {
-                    // stop server gracefully
-                    executor::block_on(srv.stop(true));
-                    break;
-                }
-                "HUP" => {}
-                _ => {
-                    // wait 10ms if recv error
-                    std::thread::sleep(std::time::Duration::from_millis(10));
+    let (_, server) = warp::serve(handle_command).bind_with_graceful_shutdown(
+        ([0, 0, 0, 0], args.port),
+        async move {
+            while let Some(res) = rx.recv().await {
+                match res {
+                    "TERM" => break,
+                    // TODO:(everpcpc) impl reload
+                    "HUP" => break,
+                    _ => log::error!("received invalid signal: {}", res),
                 }
             }
-        })
-        .unwrap();
+        },
+    );
 
-    let mut stream_hup = signal(SignalKind::hangup())?;
-    let hup_tx = tx.clone();
-    actix_rt::spawn(async move {
+    let mut stream_hup = signal(SignalKind::hangup()).unwrap();
+    let mut hup_tx = tx.clone();
+    tokio::task::spawn(async move {
         loop {
             stream_hup.recv().await;
-            // TODO:(everpcpc) impl reload
             log::info!("SIGHUP received. Reloading...");
-            hup_tx.send("TERM").unwrap();
+            hup_tx.send("HUP").await.unwrap();
         }
     });
-    let mut stream_term = signal(SignalKind::terminate())?;
-    let term_tx = tx.clone();
-    actix_rt::spawn(async move {
-        loop {
-            stream_term.recv().await;
-            log::info!("SIGTERM received. Terminating...");
-            term_tx.send("TERM").unwrap();
-        }
+    let mut stream_term = signal(SignalKind::terminate()).unwrap();
+    let mut term_tx = tx.clone();
+    tokio::task::spawn(async move {
+        stream_term.recv().await;
+        log::info!("SIGTERM received. Terminating...");
+        term_tx.send("TERM").await.unwrap();
     });
 
-    server.await
+    tokio::task::spawn(server).await.unwrap()
 }
 
-#[get("command/{command}")]
 async fn handlers_command(
-    command: web::Path<String>,
-    opts: web::Query<CommandParams>,
-    configs: web::Data<Configs>,
-) -> impl Responder {
+    command: String,
+    opts: CommandParams,
+    configs: Arc<Configs>,
+) -> Result<Box<dyn warp::Reply>, std::convert::Infallible> {
     let chunked = match opts.chunked {
         None => false,
         Some(c) => c != 0,
@@ -125,95 +115,79 @@ async fn handlers_command(
         None => Vec::new(),
         Some(a) => a.split(" ").map(|x| x.to_string()).collect(),
     };
-
-    if chunked {
-        handle_command_chunked(command.as_str(), arguments, configs)
-    } else {
-        handle_command_no_chunked(command.as_str(), arguments, configs)
-    }
-}
-
-fn handle_command_chunked(
-    command: &str,
-    arguments: Vec<String>,
-    configs: web::Data<Configs>,
-) -> HttpResponse {
-    match configs.get(command) {
-        None => HttpResponse::BadRequest().body(format!(
-            "0> {}\n",
-            CommandResult::err(format!("Unknown Command: {}", command)).to_json()
-        )),
+    match configs.get(&command) {
+        None => {
+            let err = CommandResult::err(format!("Unknown Command: {}", command));
+            Ok(Box::new(warp::reply::with_status(
+                warp::reply::json(&err),
+                StatusCode::BAD_REQUEST,
+            )))
+        }
         Some(cmd) => {
-            let (tx_cmd, rx_cmd) = std::sync::mpsc::channel::<String>();
-            let waker = Arc::new(Mutex::new(RedarrowWaker::new()));
-            let cmd = cmd.clone();
-            let mut wake_sender = waker.clone();
-            match std::thread::Builder::new()
-                .name(format!("runner for {}", command))
-                .spawn(move || {
-                    let ret = format!(
-                        "0> {}\n",
-                        cmd.execute_iter(arguments, tx_cmd.clone(), &mut wake_sender)
-                            .unwrap_or_else(|err| CommandResult::err(format!("{}", err)))
-                            .to_json()
-                    );
-                    match tx_cmd.send(ret) {
-                        Err(e) => {
-                            log::warn!("send command result error: {}", e);
-                            return;
-                        }
-                        Ok(()) => {
-                            if let Ok(mut waker) = wake_sender.lock() {
-                                waker.wake();
-                            } else {
-                                log::warn!("waker on command result failed to get lock");
-                            }
-                        }
-                    }
-                    // NOTE:(everpcpc) force end recv rx_cmd, do not wait for stdout/stderr
-                    match tx_cmd.send("\0".to_string()) {
-                        Err(e) => {
-                            log::warn!("send command end error: {}", e);
-                            return;
-                        }
-                        Ok(()) => {
-                            // NOTE:(everpcpc) acturally this wake always false
-                            if let Ok(mut waker) = wake_sender.lock() {
-                                waker.wake();
-                            } else {
-                                log::warn!("waker on command end failed to get lock");
-                            }
-                        }
-                    }
-                }) {
-                Ok(_) => HttpResponse::Ok().streaming(ChunkedResponse {
-                    rx: rx_cmd,
-                    waker: waker,
-                }),
-                Err(e) => HttpResponse::InternalServerError()
-                    .json(CommandResult::err(format!("Failed to start task: {}", e))),
+            if chunked {
+                let cmd = cmd.clone();
+                handle_command_chunked(cmd, arguments)
+            } else {
+                let r = cmd
+                    .execute(arguments)
+                    .unwrap_or_else(|err| CommandResult::err(format!("{}", err)));
+                Ok(Box::new(warp::reply::with_status(
+                    warp::reply::json(&r),
+                    StatusCode::OK,
+                )))
             }
         }
     }
 }
 
-fn handle_command_no_chunked(
-    command: &str,
+fn handle_command_chunked(
+    cmd: Command,
     arguments: Vec<String>,
-    configs: web::Data<Configs>,
-) -> HttpResponse {
-    match configs.get(command) {
-        None => {
-            let err = CommandResult::err(format!("Unknown Command: {}", command));
-            HttpResponse::BadRequest().json(err)
+) -> Result<Box<dyn warp::Reply>, std::convert::Infallible> {
+    let (tx_cmd, rx_cmd) = std::sync::mpsc::channel::<String>();
+    let waker = Arc::new(Mutex::new(RedarrowWaker::new()));
+    let mut wake_sender = waker.clone();
+    let _child = std::thread::spawn(move || {
+        let ret = format!(
+            "0> {}\n",
+            cmd.execute_iter(arguments, tx_cmd.clone(), &mut wake_sender)
+                .unwrap_or_else(|err| CommandResult::err(format!("{}", err)))
+                .to_json()
+        );
+        match tx_cmd.send(ret) {
+            Err(e) => {
+                log::warn!("send command result error: {}", e);
+                return;
+            }
+            Ok(()) => {
+                if let Ok(mut waker) = wake_sender.lock() {
+                    waker.wake();
+                } else {
+                    log::warn!("waker on command result failed to get lock");
+                }
+            }
         }
-        Some(cmd) => {
-            let r = cmd
-                .execute(arguments)
-                .unwrap_or_else(|err| CommandResult::err(format!("{}", err)));
-            HttpResponse::Ok().json(r)
+        match tx_cmd.send("\0\0".to_string()) {
+            Err(e) => {
+                log::warn!("send command end error: {}", e);
+                return;
+            }
+            Ok(()) => {
+                if let Ok(mut waker) = wake_sender.lock() {
+                    waker.wake();
+                } else {
+                    log::warn!("waker on command end failed to get lock");
+                }
+            }
         }
-    }
+    });
+    let r = ChunkedResponse {
+        rx: rx_cmd,
+        waker: waker,
+    };
+    let mut response = hyper::Response::new(hyper::Body::empty());
+    *response.body_mut() = hyper::Body::wrap_stream(r);
+    Ok(Box::new(response))
 }
 
 #[derive(Debug)]
@@ -222,8 +196,10 @@ struct ChunkedResponse {
     waker: Arc<Mutex<RedarrowWaker>>,
 }
 
+unsafe impl Sync for ChunkedResponse {}
+
 impl Stream for ChunkedResponse {
-    type Item = Result<bytes::Bytes, actix_web::Error>;
+    type Item = Result<bytes::Bytes, warp::Error>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         match self.rx.try_recv() {
@@ -236,7 +212,7 @@ impl Stream for ChunkedResponse {
                 Poll::Pending
             }
             Ok(result) => {
-                if result == "\0" {
+                if result == "\0\0" {
                     Poll::Ready(None)
                 } else {
                     Poll::Ready(Some(Ok(Bytes::from(result))))
